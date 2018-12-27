cca9bab5ea59f833f656c29580e4016f9af5c0dc
Steven-N-Hart <hart.steven@mayo.edu>
2018-12-27 13:30:41 -0600

    added tfrecord structure

--- scripts/dataset_utils.py
+++ scripts/dataset_utils.py
@@ -66,45 +66,58 @@ def float_feature(values):
   return tf.train.Feature(float_list=tf.train.FloatList(value=values))
 
 
-def image_to_tfexample(image_data, image_format, height, width, class_id):
+def image_to_tfexample(image_data, image_format, height, width,
+			race=None,
+			ajcc_pathologic_tumor_stage=None,
+			pam50mRNA=None,
+			histological_type=None,
+			tissue_pathology=None,
+			tumor_class=None,
+			tumor_status=None,
+			DeadInFiveYrs=None,
+			ER_Status=None,
+			PR_Status=None,
+			HER2_Status=None,
+			Metastasis_Coded=None,
+			ATM_Mutations=0,
+			BRCA1_Mutations=0,
+			BRCA2_Mutations=0,
+			CDH1_Mutations=0,
+			CDKN2A_Mutations=0,
+			PTEN_Mutations=0,
+			TP53_Mutations=0,
+			AnyGene_Mutations=0
+			):
   return tf.train.Example(features=tf.train.Features(feature={
       'image/encoded': bytes_feature(image_data),
       'image/format': bytes_feature(image_format),
-      'image/class/label': int64_feature(class_id),
       'image/height': int64_feature(height),
       'image/width': int64_feature(width),
+      'phenotype/race': int64_feature(race),
+      'phenotype/ajcc_pathologic_tumor_stage': int64_feature(ajcc_pathologic_tumor_stage),
+      'phenotype/pam50mRNA': int64_feature(pam50mRNA),
+      'phenotype/histological_type': int64_feature(histological_type),
+      'phenotype/tissue_pathology': int64_feature(tissue_pathology),
+      'phenotype/tumor_class': int64_feature(tumor_class),
+      'phenotype/tumor_status': int64_feature(tumor_status),
+      'phenotype/DeadInFiveYrs': int64_feature(DeadInFiveYrs),
+      'phenotype/ER_Status': int64_feature(ER_Status),
+      'phenotype/PR_Status': int64_feature(PR_Status),
+      'phenotype/HER2_Status': int64_feature(HER2_Status),
+      'phenotype/Metastasis_Coded': int64_feature(Metastasis_Coded),
+      'phenotype/ATM_Mutations': int64_feature(ATM_Mutations),
+      'phenotype/BRCA1_Mutations': int64_feature(BRCA1_Mutations),
+      'phenotype/BRCA2_Mutations': int64_feature(BRCA2_Mutations),
+      'phenotype/CDH1_Mutations': int64_feature(CDH1_Mutations),
+      'phenotype/CDKN2A_Mutations': int64_feature(CDKN2A_Mutations),
+      'phenotype/PTEN_Mutations': int64_feature(PTEN_Mutations),
+      'phenotype/TP53_Mutations': int64_feature(TP53_Mutations),
+      'phenotype/AnyGene_Mutations': int64_feature(AnyGene_Mutations),
+
   }))
 
 
 
-def write_label_file(labels_to_class_names, dataset_dir,
-                     prefix=_PREFIX,
-                     filename=_LABELS_FILENAME):
-  """Writes a file with the list of class names.
-
-  Args:
-    labels_to_class_names: A map of (integer) labels to class names.
-    dataset_dir: The directory in which the labels file should be written.
-    filename: The filename where the class names are written.
-  """
-  labels_filename = os.path.join(dataset_dir, _PREFIX + "_" +filename)
-  with tf.gfile.Open(labels_filename, 'w') as f:
-    for label in labels_to_class_names:
-      class_name = labels_to_class_names[label]
-      f.write('%d:%s\n' % (label, class_name))
-
-
-def has_labels(dataset_dir, filename=LABELS_FILENAME):
-  """Specifies whether or not the dataset directory contains a label map file.
-
-  Args:
-    dataset_dir: The directory in which the labels file is found.
-    filename: The filename where the class names are written.
-
-  Returns:
-    `True` if the labels file exists and `False` otherwise.
-  """
-  return tf.gfile.Exists(os.path.join(dataset_dir, filename))
 
 
 def read_label_file(dataset_dir, filename=LABELS_FILENAME):
